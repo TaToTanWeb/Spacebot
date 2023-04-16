9b145445d41d47633380c08c68244f17d8201ecf
TaToTanWeb <75488098+TaToTanWeb@users.noreply.github.com>
2023-04-16 16:22:23 +0200

    Add files via upload

--- Discovery.md
+++ Discovery.md
@@ -1,8 +1,16 @@
 ---
-app_name: "Spacebot"
-title: "Create your own telegram bot."
-tagline: "Create and deploy your telegram bot on your own Space instance."
-theme_color: "#78feed"
-git: "https://github.com/deta/deta"
+app_name: Spacebot
+tagline: Create your own telegram bot.
+theme_color: '#78feed'
+git: https://github.com/TaToTanWeb/Spacebot
 ---
 
+Spacebot lets you create your own telegram bot, and automatically deploys it on your own Space App instance using webhooks.
+
+### Getting started
+- Talk to [BotFather](https://t.me/BotFather) and create a new bot
+- Set the bot's name and profile image as you wish
+- Open the app and paste the bot token
+- Define responses for any command (such as /start, /help, etc)
+- Click on "save" and wait for the webhook to be set
+- Enjoy your bot!
--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from src.handle_update import handle_update
 
 
 class Data(BaseModel):
+    # used to recieve JSON body of POST requests
     token: str
     patterns: dict
 
@@ -17,23 +18,31 @@ class Data(BaseModel):
 class App(FastAPI):
     def __init__(self):
         FastAPI.__init__(self)
+        # automatically serving all files in the "public" folder
         self.mount('/public', StaticFiles(directory = 'public'), name = 'public')
+        # base url for telegram bots api (https://core.telegram.org/bots/api)
         self.url = "https://api.telegram.org/bot{}/{}"
+        # setting up the deta base sdk (https://deta.space/docs/en/reference/base/sdk)
         self.config = Deta().Base('config')
 
+        # Routes
+        # / — view the main app interface
         @self.get('/')
         def home():
             content = open('index.html', 'r').read()
             return HTMLResponse(content)
         
+        # /save — store configuration and set the webhook
         @self.post('/save')
         def save(data: Data):
             return save_config(self, data)
 
+        # /update (public) — handle telegram updates
         @self.post('/update/{bot_id:path}')
         async def update(request: Request, bot_id: str):
             return await handle_update(self, request, bot_id = bot_id)
 
+        # /favicon.ico — returns the favicon for browsers
         @self.post('/favicon.ico')
         def favicon():
             return FileResponse('favicon.ico')
